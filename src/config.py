dd0bcf3b49e1585f82b5c8f70d395f0a7c67cc01
ncheymbamalu <nchey.learnings@gmail.com>
2025-02-16 15:40:26 -0800

    adding the configuration and logging modules

---
+++ src/config.py
@@ -0,0 +1,38 @@
+"""This module sets up the project's configurations."""
+
+from pathlib import Path, PosixPath
+
+from omegaconf import DictConfig, OmegaConf
+
+
+class Paths:
+    """Configuration for the project's primary directories and filepaths.
+
+    Attributes:
+        PROJECT_DIR (PosixPath): Project's root directory.
+        DATA_DIR (PosixPath): Project's data directory, ~/data/.
+        ARTIFACTS_DIR (PosixPath): Project's artifacts directory, ~/artifacts/.
+        LOGS_DIR (PosixPath): Project's logs directory, ~/logs/.
+        ENV (PosixPath): Project's .env file path, ~/.env.
+        CONFIG (PosixPath): Project's configuration file path, ~/config.yaml.
+        MODEL (PosixPath): Project's trained ML model file path, ~/artifacts/model.pkl.
+    """
+    PROJECT_DIR: PosixPath = Path(__file__).parent.parent.absolute()
+    DATA_DIR: PosixPath = PROJECT_DIR / "data"
+    ARTIFACTS_DIR: PosixPath = PROJECT_DIR / "artifacts"
+    LOGS_DIR: PosixPath = PROJECT_DIR / "logs"
+    ENV: PosixPath = PROJECT_DIR / ".env"
+    CONFIG: PosixPath = PROJECT_DIR / "config.yaml"
+    MODEL: PosixPath = ARTIFACTS_DIR / "model.pkl"
+
+
+def load_config() -> DictConfig:
+    """Loads Paths.CONFIG as a DictConfig object.
+
+    Returns:
+        DictConfig: Dictionary-like object with user-defined key-values pairs.
+    """
+    try:
+        return OmegaConf.load(Paths.CONFIG)
+    except Exception as e:
+        raise e
